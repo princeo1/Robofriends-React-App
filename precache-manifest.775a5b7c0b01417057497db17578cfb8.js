0e1f9f39600804332c486d31735f0eeae2264459
Prince Soni <1996princesoni@gmail.com>
2021-07-13 20:36:24 +0530

    Updates

---
+++ precache-manifest.775a5b7c0b01417057497db17578cfb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4df2800470c40fa449a4b277fbe4d0",
+    "url": "/Robofriends-React-App/index.html"
+  },
+  {
+    "revision": "8d9e075fff116ae639ee",
+    "url": "/Robofriends-React-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "527db11d96528e068009",
+    "url": "/Robofriends-React-App/static/css/main.cbef6ff4.chunk.css"
+  },
+  {
+    "revision": "8d9e075fff116ae639ee",
+    "url": "/Robofriends-React-App/static/js/2.b902440d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends-React-App/static/js/2.b902440d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527db11d96528e068009",
+    "url": "/Robofriends-React-App/static/js/main.2cc8ba61.chunk.js"
+  },
+  {
+    "revision": "d2779408ee2e0c464bcc",
+    "url": "/Robofriends-React-App/static/js/runtime-main.f17e205e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-React-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
